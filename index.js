7743ec0783893e8deac2e87dd5a731265b216a51
sai3prasath <saprasath.3@gmail.com>
2020-05-15 04:18:59 +0530

    Changes #1

--- index.js
+++ index.js
@@ -73,11 +73,14 @@ wss.on('connection', function connection(ws, req) {
   wsclient.on('message', function incoming(data) {
     var parsed = JSON.parse(data);
     isMobileView = parsed.isMobileView;
+    patientData = data.patientdata;
+    userName = data.userName;
+    isConsultationVisible = data.isConsultationVisible;
     console.log('received: %s', parsed.isMobileView);
-    // wsclient.send(JSON.stringify({ data: parsed, isMobileView: isMobileView }));
-    for (var i = 0; i < wsclients.length; i++) {
-      wsclients[i].ws.send(JSON.stringify({ data: parsed, isMobileView: isMobileView }));
-    }
+    wsclient.send(JSON.stringify({ patientdata: patientData,username:userName,isConsultationVisible:isConsultationVisible, isMobileView: isMobileView }));
+//     for (var i = 0; i < wsclients.length; i++) {
+//       wsclients[i].ws.send(JSON.stringify({ data: parsed, isMobileView: isMobileView }));
+//     }
     getData(parsed);
   });
   ws.isAlive = true;
